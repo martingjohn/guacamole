979b391f53a90e4f933e59db93ea557fe3e6e616
martingjohn <20387863+martingjohn@users.noreply.github.com>
2017-08-10 18:24:42 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Start tomcat7 - although it fails
+echo "Tomcat7 start script says it fails to start even when it's successful"
+service tomcat7 start
+service guacd start
+
+sleep infinity
